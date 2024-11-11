0f29f1cb5f25ca02949a425d1ded319975a101cf
Pavel Falin <165943002+falinpavel@users.noreply.github.com>
2024-11-11 17:17:16 +0300

    add logger

--- utils/http_all_methods.py
+++ utils/http_all_methods.py
@@ -1,3 +1,5 @@
+from utils.logger import Logger
+
 import requests
 
 """
@@ -17,21 +19,29 @@ class HttpAllMethods:
 
     @staticmethod  # статический метод. Проставляем, нужно обращаться к нему без создания экземпляра
     def get(url):  # не передаем self в качестве аргумента, так как делаем наши методы статическими
+        Logger.add_request(url, "GET")
         result = requests.get(url, headers=HttpAllMethods.headers, cookies=HttpAllMethods.cookies)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def post(url, body):  # обязательно передаем body
+        Logger.add_request(url, "POST")
         result = requests.post(url, json=body, headers=HttpAllMethods.headers, cookies=HttpAllMethods.cookies)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def put(url, body):  # обязательно передаем body
+        Logger.add_request(url, "PUT")
         result = requests.put(url, json=body, headers=HttpAllMethods.headers, cookies=HttpAllMethods.cookies)
+        Logger.add_response(result)
         return result
 
     @staticmethod
     def delete(url, body):
+        Logger.add_request(url, "DELETE")
         result = requests.delete(url, json=body, headers=HttpAllMethods.headers, cookies=HttpAllMethods.cookies)
+        Logger.add_response(result)
         return result
 
